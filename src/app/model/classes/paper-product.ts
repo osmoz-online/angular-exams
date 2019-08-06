773b32bc8a4f6b16ebdf3ac30a072577cebe8897
osmoz-online <osmoz.online@gmail.com>
2019-08-06 21:47:37 +0200

    sets up class and service, inits components layout

--- src/app/model/classes/paper-product.ts
+++ src/app/model/classes/paper-product.ts
@@ -1,2 +1,17 @@
 export class PaperProduct {
+    public id: number;
+    public nom: string;
+    public texture: string;
+    public grammage: string;
+    public couleur: string;
+
+    constructor(data?) {
+        if (data) {
+            if (data.hasOwnProperty('id')) this.id = data['id'];
+            if (data.hasOwnProperty('nom')) this.nom = data['nom'];
+            if (data.hasOwnProperty('texture')) this.texture = data['texture'];
+            if (data.hasOwnProperty('grammage')) this.grammage = data['grammage'];
+            if (data.hasOwnProperty('couleur')) this.couleur = data['couleur'];
+        }
+    }
 }
--- src/app/model/services/paper-repo.service.ts
+++ src/app/model/services/paper-repo.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { PaperProduct } from '../classes/paper-product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaperRepoService {
 
+  private products = [
+    new PaperProduct({
+      id: 1,
+      nom: 'Canson Figueras (huile & acrylique)',
+      texture: 'Toilée',
+      grammage: '290g/m2',
+      couleur: 'Blanc'
+    }),
+    new PaperProduct({
+      id: 2,
+      nom: 'Canson Montval (aquarelle)',
+      texture: 'Micro-perforée',
+      grammage: '270g/m2',
+      couleur: 'Blanc'
+    }),
+    new PaperProduct({
+      id: 3,
+      nom: 'Clairefontaine PastelMat',
+      texture: 'Veloutée',
+      grammage: '360g/m2',
+      couleur: 'Blanc'
+    }),
+    new PaperProduct({
+      id: 4,
+      nom: 'Clairefontaine Paint\'on',
+      texture: 'Bouffante',
+      grammage: '250g/m2',
+      couleur: 'Blanc'
+    }),
+    new PaperProduct({
+      id: 5,
+      nom: 'Dalbe dessin couleur',
+      texture: 'Grain léger',
+      grammage: '170g/m2',
+      couleur: 'Lavande'
+    })
+  ];
+
   constructor() { }
+
+  public getProducts() {
+    return this.products;
+  }
 }
