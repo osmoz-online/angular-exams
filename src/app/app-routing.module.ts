c9f6abae812121e2652d659676bc786ab617af09
osmoz-online <osmoz.online@gmail.com>
2019-08-25 21:49:27 +0200

    adds routes (+ data properties) and reorders them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ProductListComponent } from './views/product-list/product-list.componen
 import { ProductDetailComponent } from './views/product-detail/product-detail.component';
 import { PaperUiComponent } from './views/paper-ui/paper-ui.component';
 import { PaperMenuComponent } from './views/paper-menu/paper-menu.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', component: PaperMenuComponent, data: { linkLabel: "", show: false } },
-  { path: 'global', component: PaperUiComponent, data: { linkLabel: "App globale", show: true } },
-  { path: 'produits', component: ProductListComponent, data: { linkLabel: "Liste", show: true } },
-  { path: 'produits/:id', component: ProductDetailComponent, data: { linkLabel: "Détail", show: false } }
+  { path: 'global', component: PaperUiComponent, data: { show: true, linkLabel: "App globale", selectDefault: true } },
+  { path: 'produits', component: ProductListComponent, data: { show: true, linkLabel: "Liste", navigate: true } },
+  { path: 'produits/:id', component: ProductDetailComponent, data: { show: false } },
+  { path: '', component: PaperMenuComponent, data: { show: false } },
+  { path: '**', component: PageNotFoundComponent, data: { show: false } }
 ];
 
 @NgModule({
