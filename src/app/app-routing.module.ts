c7a7d3b62ac4c78dfb74994bfcf32bfe2cf36935
osmoz-online <osmoz.online@gmail.com>
2019-08-26 22:54:57 +0200

    replaces 2 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductListComponent } from './views/product-list/product-list.component';
-import { ProductDetailComponent } from './views/product-detail/product-detail.component';
 import { PaperUiComponent } from './views/paper-ui/paper-ui.component';
 import { PaperMenuComponent } from './views/paper-menu/paper-menu.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { ListAndAddComponent } from './views/list-and-add/list-and-add.component';
+import { DetailAndReturnComponent } from './views/detail-and-return/detail-and-return.component';
 
 const routes: Routes = [
   { path: 'global', component: PaperUiComponent, data: { show: true, linkLabel: "App globale", selectDefault: true } },
-  { path: 'produits', component: ProductListComponent, data: { show: true, linkLabel: "Liste", navigate: true } },
-  { path: 'produits/:id', component: ProductDetailComponent, data: { show: false } },
+  { path: 'produits', component: ListAndAddComponent, data: { show: true, linkLabel: "Liste", navigate: true } },
+  { path: 'produits/:id', component: DetailAndReturnComponent, data: { show: false } },
   { path: '', component: PaperMenuComponent, data: { show: false } },
   { path: '**', component: PageNotFoundComponent, data: { show: false } }
 ];
