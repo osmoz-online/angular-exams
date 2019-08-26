f7a6f804b3ff8ef4b02b3773ea32854882d963d5
osmoz-online <osmoz.online@gmail.com>
2019-08-27 00:09:23 +0200

    improves id handling to display suitable product

--- src/app/views/product-detail/product-detail.component.ts
+++ src/app/views/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PaperProduct } from 'src/app/model/classes/paper-product';
 import { ActivatedRoute } from '@angular/router';
 import { PaperRepoService } from 'src/app/model/services/paper-repo.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,6 +11,7 @@ import { PaperRepoService } from 'src/app/model/services/paper-repo.service';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  private _formValueChangesSubscription: Subscription;
   private productToDisplay: PaperProduct;
   private isFromUser: boolean = true;
 
@@ -34,14 +36,28 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit() {
     const id = + this._activatedRoute.snapshot.paramMap.get('id');
-    if (id !== 0) {
+    if (id == 0) {
+      this.productToDisplay = this._paperRepo.getProduct(1);
+    } else if (id > 0) {
       this.productToDisplay = this._paperRepo.getProduct(id);
-      this.setProduct(this.productToDisplay);
+    } else {
+      this.productToDisplay = new PaperProduct({
+        id: -1,
+        nom: '',
+        texture: '',
+        grammage: '',
+        couleur: ''
+      });
     }
-    this.productForm.valueChanges.subscribe((formValue) => {
+    this.setProduct(this.productToDisplay);
+    this._formValueChangesSubscription = this.productForm.valueChanges.subscribe((formValue) => {
       if (this.isFromUser)
         this.productInEdition.emit(true);
     });
+  }
+
+  ngOnDestroy() {
+    this._formValueChangesSubscription.unsubscribe();
   }
 
   setProduct(p: PaperProduct) {
